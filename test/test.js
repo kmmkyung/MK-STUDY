a8a8a4287155c08185475fab3fe2b81093d40be2
MK <01aldrn10@gmail.com>
2023-07-11 16:24:46 +0900

    ㄴ

--- test/test.js
+++ test/test.js
@@ -1,47 +1,26 @@
+let 사람 ={
+  name:'aaa',
+  sayHi:function(){
+    console.log('안녕 나는 ' + this.name);
+  }
+}
 
-// function upresize(){
-//   let winH = window.innerWidth;
-//   const body = document.querySelector(".color")
-//   // console.log("가로",winH);
-//   if( winH < 400){
-//     body.classList.add("blue")
-//     body.classList.remove("purple")
-//     body.classList.remove("gold")
-//   }
-//   else if(400 <= winH && winH <800){
-//     body.classList.add("purple")
-//     body.classList.remove("blue")
-//   }
-//   else{
-//     body.classList.remove("blue")
-//     body.classList.remove("purple")
-//     body.classList.add("gold")
-//   }
-// }
+사람.sayHi()
+
+let 자료 = {
+  data : [1,2,3,4,5],
+  전부더하기 : function(){
+    let add = 0;
+    this.data.forEach(function(v){
+      console.log('v',v);
+      
+      add = v + add
+    });
+    console.log(add);
     
-// window.addEventListener("resize",upresize)
-
-
-document.querySelector(".select_1").addEventListener("input",function(){
-
-  const val = document.querySelector(".select_1").value
-  const se1 = document.querySelector(".select_1")
-  const se2 = document.querySelector(".select_2")
-
-  console.log('val',val);
-  
-  if(val == "셔츠"){
-    se2.classList.remove("hidden");
   }
+};
 
-  else if(val == "바지"){
-    se2.classList.remove("hidden");
-    const push = `<option>100</option><option>105</option>`;
-    se2.innerHTML=push
-  }
+자료.전부더하기()
 
-  else{
-    document.querySelector(".select_2").classList.add("hidden");
-  }
 
-})
