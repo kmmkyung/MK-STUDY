b9ef94ab3f84b29a01749efc044ee3e883e9d287
MK <01aldrn10@gmail.com>
2023-07-14 22:22:48 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,33 +1,11 @@
+const player = {
+  name: "suyoung",
+  points: 120,
+  fat: "little bit",
+  tall: false
+}
 
-// function---------------------------------
-// function additem(items){
-//   return items + ten;
-// }
-
-// const effect1 = additem(item1) 
-// const effect2 = additem(item2)
-// const effect3 = additem(item3)
-// console.log(effect1,effect2,effect3); // 11, 12, 13
-
-// function---------------------------------
-// const additem = function(items){
-//   return items + ten;
-// }
-
-// const effect1 = additem(item1)
-// const effect2 = additem(item2)
-// const effect3 = additem(item3)
-// console.log(effect1,effect2,effect3); // 11, 12, 13
-
-
-
-
-const item1 = 1;
-const item2 = 2;
-const item3 = 3;
-const ten = 10;
-
-// function---------------------------------
-const additem = items => items + ten
-
-
+for(let key in player){
+  console.log(key); // name, points, fat, tall 값 출력
+	console.log(player[key]); // suyoung, 120, little bit, false
+}
