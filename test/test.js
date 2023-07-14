badda0920dc7cabd1abb967231cadc8a6a489f46
MK <01aldrn10@gmail.com>
2023-07-14 21:46:54 +0900

    s

---
+++ test/test.js
@@ -0,0 +1,33 @@
+
+// function---------------------------------
+// function additem(items){
+//   return items + ten;
+// }
+
+// const effect1 = additem(item1) 
+// const effect2 = additem(item2)
+// const effect3 = additem(item3)
+// console.log(effect1,effect2,effect3); // 11, 12, 13
+
+// function---------------------------------
+// const additem = function(items){
+//   return items + ten;
+// }
+
+// const effect1 = additem(item1)
+// const effect2 = additem(item2)
+// const effect3 = additem(item3)
+// console.log(effect1,effect2,effect3); // 11, 12, 13
+
+
+
+
+const item1 = 1;
+const item2 = 2;
+const item3 = 3;
+const ten = 10;
+
+// function---------------------------------
+const additem = items => items + ten
+
+
