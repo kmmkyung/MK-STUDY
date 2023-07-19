113e0a0c69fed8c86d74d19ea5bdca1d0b95ca92
MK <01aldrn10@gmail.com>
2023-07-20 03:21:59 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,13 @@
-const mon = "mon"
-const tue = "tue"
-const wed = "wed"
-const thu = "thu"
-const fri = "fri"
-const sat = "sat"
+const nameAll = {
+	people : '2',
+	firstname : function(){
+		return 'kim su young'
+	},
+	lastname : () => {
+		console.log('kim min kyung');
+	}
+}
+
+console.log(nameAll.firstname()); // kim su young
+nameAll.lastname(); //kim min kyung
+//함수 자체가 담겼기 때문에 실행()을 해야 한다!
