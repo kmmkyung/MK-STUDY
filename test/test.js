ceebe8bfcd37c044119f2e2d15d596858e2d480d
MK <01aldrn10@gmail.com>
2023-07-22 15:15:16 +0900

    s

---
+++ test/test.js
@@ -0,0 +1,31 @@
+// 숫자형을 문자형으로 변환하기
+// function nonum(n){
+//     let answer = '';
+//     answer = String(n)
+//     return answer;
+// }
+// console.log(nonum(123));
+// console.log(typeof(nonum(567)));
+
+function ENG(alphabet){
+    return alphabet.toUpperCase();
+}
+
+console.log(ENG("AbCdEfG"));
+
+// function aa(n) {
+//     return n+"";
+// }
+
+
+// Number()
+// String()
+
+// function a(start, end) {
+//     var answer = [];
+//     for( let i=start; i>=end; i-- ){
+//         answer.push(i)
+//     }
+//     return answer;
+// }
+// console.log(a(10,3));
