2ffd0cb678cc77aa44c8e0ce34a1809be0dbebc9
MK <01aldrn10@gmail.com>
2023-07-24 17:52:07 +0900

    s

--- test/test.js
+++ test/test.js
@@ -18,10 +18,3 @@ console.log(o2);
 var o1 = { a : 1, b : 2};
 var o2 = { a : 3, ...o1 };
 console.log(o2);
-
-const d = new Date();
-
-console.log(new Date());
-console.log(new Date().toISOString())
-console.log(new Date().toUTCString())
-console.log(new Date().toTimeString())
