3f8c7c7f94e75bf256fb32c9f9749100a04cc1a8
MK <01aldrn10@gmail.com>
2023-08-02 00:54:47 +0900

    s

---
+++ :PaintApp_clon/app.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded',function(){
+  const canvas = document.querySelector('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 800;
+  canvas.height = 800;
+
+
+
+
+
+  
+
+})
