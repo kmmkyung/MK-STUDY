644cd92857d4e10f34614a263890d1b6771bad47
MK <01aldrn10@gmail.com>
2023-08-02 16:44:50 +0900

    s

--- :PaintApp_clon/app.js
+++ :PaintApp_clon/app.js
@@ -1,13 +1,35 @@
 window.addEventListener('DOMContentLoaded',function(){
+  const lineWidth = document.querySelector('#line-width')  
   const canvas = document.querySelector('canvas');
   const ctx = canvas.getContext('2d');
   canvas.width = 800;
   canvas.height = 800;
-
-
-
-
-
+  let isPainting = false;
+  ctx.lineWidth = lineWidth.value;
   
+  function onMove(event){
+    if(isPainting){
+      ctx.lineTo(event.offsetX, event.offsetY);
+      ctx.stroke();
+      return;
+    }
+    ctx.beginPath();
+    ctx.moveTo(event.offsetX, event.offsetY);
+  }
+  function startPainting(){
+    isPainting = true;
+  }
+  function cancelPainting(){
+    isPainting = false;
+  }
+  canvas.addEventListener('mousemove',onMove);
+  canvas.addEventListener('mousedown',startPainting);
+  canvas.addEventListener('mouseup',cancelPainting);
+  canvas.addEventListener('mouseleave',cancelPainting);
 
+  function onLineWidthChange(event){
+    ctx.lineWidth=event.target.value;
+    
+  }
+  lineWidth.addEventListener('change',onLineWidthChange);
 })
