5a90cf1181176df06e9d66ed6154df6f44287d25
MK <01aldrn10@gmail.com>
2023-08-10 22:30:47 +0900

    s

--- FastCampus/AIO/1.STARBUCKS/js/main.js
+++ FastCampus/AIO/1.STARBUCKS/js/main.js
@@ -30,4 +30,37 @@ window.addEventListener('DOMContentLoaded',function(){
     autoplay: true,
     loop: true
   });
+
+  new Swiper('.promotion .swiper',{
+    // direction:'horizontal', // 기본값
+    slidesPerView :3, // 한번에 보여줄 슬라이드 개수
+    spaceBetween:10, // 슬라이드 사이 여백
+    centeredSlides:true, // 1번 슬라이드가 가운데 보이게
+    autoplay: {
+      delay: 3000
+    },
+    loop: true,
+    pagination:{
+      el:'.swiper-pagination', // 페이지 번호 선택자
+      clickable: true, // 사용자의 페이지 번호 요소 제어
+    },
+    navigation:{
+      prevEl:'.swiper-prev',
+      nextEl:'.swiper-next'
+    }
+  });
+
+  const promotionEl = document.querySelector('.promotion');
+  const promotionToggleBtn = document.querySelector('.toggle-promotion');
+  let isHidePromotoin = false;
+
+  promotionToggleBtn.addEventListener('click',function(){
+    isHidePromotoin = !isHidePromotoin
+    if(isHidePromotoin){
+      promotionEl.classList.add('hide')
+    }
+    else{
+      promotionEl.classList.remove('hide')
+    }
+  })
 })
