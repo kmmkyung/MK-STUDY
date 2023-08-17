add6061710af9754689a04addaf8e44936ddcc8e
MK <01aldrn10@gmail.com>
2023-08-17 17:41:54 +0900

    s

--- FastCampus/AIO/1.STARBUCKS/js/main.js
+++ FastCampus/AIO/1.STARBUCKS/js/main.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded',function(){
   const badgeEl = document.querySelector('.badges')
   window.addEventListener('scroll',_.throttle(function(){
-    console.log(window.scrollY);
     if(window.scrollY > 500){
       // gsap.to(요소,지속시간,옵션(객체))
       gsap.to(badgeEl, 0.6,{
