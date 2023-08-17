e1d4a20902c14bc5fb3a862b4f10965f2cf8deab
MK <01aldrn10@gmail.com>
2023-08-17 20:45:41 +0900

    s

---
+++ ::Study/test.js
@@ -0,0 +1,21 @@
+// document.getElementById('input').onkeyup = _.debounce(function() {
+//   document.getElementById('debounced').innerText = 'Debounced message!';
+// }, 500);
+
+const input = document.querySelector('input');
+const keyevent = document.querySelector('.event');
+const debounce = document.querySelector('.debounce');
+let num = 0;
+let debouncednum = 0;
+
+function keyevent(){
+  keyevent.innerText=num++
+}
+
+function debounce = ._debounce(function(){
+  debounce.innerText=debouncednum++
+})
+input.addEventListener('keyup',function(){
+  keyevent()
+  debounce()
+})
