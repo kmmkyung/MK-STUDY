c418b1d8ae110a50c2cef4944033e0ab3bdec979
MK <01aldrn10@gmail.com>
2023-08-18 20:06:42 +0900

    s

--- Nomad/:Chrome_clon/js/weather.js
+++ Nomad/:Chrome_clon/js/weather.js
@@ -10,8 +10,8 @@ function GeoOk(position){
   .then((data) => {
     const weather = document.querySelector("#weather span:first-child")
     const city = document.querySelector("#weather span:last-child")
+    weather.innerText = `${data.weather[0].main} | ${data.main.temp}`
     city.innerText = data.name;
-    weather.innerText = `${data.weather[0].main}/${data.main.temp}`
   })
 }
 
