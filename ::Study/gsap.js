7ea4d50abc56ecb1ae1e44f3b29afcef4ef1b20b
MK <01aldrn10@gmail.com>
2023-08-20 02:50:57 +0900

    s

--- ::Study/gsap.js
+++ ::Study/gsap.js
@@ -1,22 +1,31 @@
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const target = document.querySelector('.box');
 
-gsap.to(target, {
-  duration: 2,
-  x: 500,
-  borderRadius: 100,
-  rotation: 360, 
-});
+// gsap.to(target, {
+//   duration: 2,
+//   x: 500,
+//   borderRadius: 100,
+//   rotation: 360, 
+// });
 
 // gsap.to("target",{
 //   scrollTrigger:{
 //     trigger:"target",
-//     // markers:"true",
-//     // start:"top 90%",
-//     // end:"bottom top"
+//     markers:"true",
 //     x:200
 //   },
 // })
+
+// 02 : trigger
+gsap.to(target, {
+  duration: 2,
+  x: 100,
+  y:100,
+  rotation: 360,
+  borderRadius: 100,
+  scrollTrigger: {
+      trigger: target,  //시작점 설정
+      markers:"true",
+  }
+});
