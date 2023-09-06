491f9444ec1b4fd3b04b44e3d1f2d51e52245258
MK <01aldrn10@gmail.com>
2023-09-06 23:53:46 +0900

    s

--- FastCampus/AIO/2.APPLE/js/main.js
+++ FastCampus/AIO/2.APPLE/js/main.js
@@ -89,3 +89,20 @@ function hideSearch(){
   searchDelayEls.reverse()
   searchInputEl.value=''
 }
+
+// 비디오 재생
+const video = document.querySelector('.stage video')
+const playBtn = document.querySelector('.controller--play')
+const pauseBtn = document.querySelector('.controller--pause')
+
+playBtn.addEventListener('click',function(){
+  video.play()
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+})
+
+pauseBtn.addEventListener('click',function(){
+  video.pause()
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+})
