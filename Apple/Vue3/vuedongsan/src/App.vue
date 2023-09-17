5da18d809030da88325318cd745d51556a0dfa71
MK <01aldrn10@gmail.com>
2023-09-17 22:51:27 +0900

    s

--- Apple/Vue3/vuedongsan/src/App.vue
+++ Apple/Vue3/vuedongsan/src/App.vue
@@ -1,27 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>dld</h1>안녕하세요
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="menu">
+      <a v-for="(a, i) in 메뉴들" :key="i">{{ a }}</a>
+      <!-- 메뉴들 갯수만큼 반복되고 a는 메뉴들의 자료로 변경된다 -->
+      <!-- 왼쪽은 하나하나의 데이터 오른쪽은 1씩 증가하는 정수가 된다 -->
+    </div>
+    <div>
+      <h4>{{ products[0] }}</h4>
+      <p>50만원</p>
+    </div>
+    <div>
+      <h4>{{ products[1] }}</h4>
+      <p>50만원</p>
+    </div>
+    <div>
+      <h4>{{ products[2] }}</h4>
+      <p>50만원</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      메뉴들: ["Home", "Shop", "About"],
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
