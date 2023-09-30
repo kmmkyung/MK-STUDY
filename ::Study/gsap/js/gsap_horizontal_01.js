14237dddb96b6c5599ee1db6cf18876672d650be
MK <01aldrn10@gmail.com>
2023-09-30 15:13:37 +0900

    s

--- ::Study/gsap/js/gsap_horizontal_01.js
+++ ::Study/gsap/js/gsap_horizontal_01.js
@@ -4,13 +4,26 @@ const container = gsap.utils.toArray('.container')
 gsap.to(arrow,{ y:10, repeat:-1, yoyo:true });
 
 gsap.to(container,{
-  xPercent: -100 * (container.length),
+  xPercent: -100 * (container.length-1),
   scrollTrigger:{
-    markers:true,
-    trigger:'main',
+    // markers:true,
+    trigger:'#main',
     pin: true,
     scrub: 1,
-    snap: 1 / (container.length-1),
-    end:'+=' + document.querySelector('#container1').offsetWidth,
+    // snap: 1 / (container.length-1),
+    end:'+=' + document.querySelector('#main').offsetWidth,
   }
 })
+
+// container.forEach((v,i) => {
+//   ScrollTrigger.create({
+//     trigger:v,
+//     start: 'bottom 20%'+ (v.offsetLeft - window.innerWidth/2) * (document.querySelector("#main").offsetWidth/(v.offsetWidth * (container.length - 1))),
+//     // end: '+=' + v.offsetWidth * (document.querySelector("#main").offsetWidth/(v.offsetWidth * (container.length - 1))),
+//     markers:true,
+//     toggleClass:{
+//       targets:v,
+//       className: 'active'
+//     }
+//   })
+// });
