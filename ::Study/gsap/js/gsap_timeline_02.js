82f7d9dddb88d8b4ecf80a6604c9668696683e97
MK <01aldrn10@gmail.com>
2023-10-02 00:36:21 +0900

    Update gsap_timeline_02.js

--- ::Study/gsap/js/gsap_timeline_02.js
+++ ::Study/gsap/js/gsap_timeline_02.js
@@ -7,21 +7,21 @@ text.forEach((v)=>{
   document.querySelector('#h1').innerHTML = texts 
   })
 
-// const tl = gsap.timeline({defaults: { ease: "power4.inOut"},
-// repeat: -1, 
-// yoyo: true, 
-// });
+const tl = gsap.timeline({defaults: { ease: "power4.inOut"},
+repeat: -1, 
+yoyo: true, 
+});
 
-// tl.from(document.querySelectorAll('span'),{
-//   y: 20,
-//   opacity: 0,
-//   skewX: 30,
-//   stagger: 0.03,
-//   duration: 1,
-// }).to(document.querySelectorAll('span'),{
-//   y: -20,
-//   skewX: 0,
-//   opacity:1,
-//   stagger:0.03,
-//   duration: 1,
-// })
+tl.from(document.querySelectorAll('span'),{
+  y: 20,
+  opacity: 0,
+  skewX: 30,
+  stagger: 0.03,
+  duration: 1,
+}).to(document.querySelectorAll('span'),{
+  y: -20,
+  skewX: 0,
+  opacity:1,
+  stagger:0.03,
+  duration: 1,
+})
