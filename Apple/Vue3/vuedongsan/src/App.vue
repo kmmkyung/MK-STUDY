b18fd3a6dabc7543c8830d92c24de85e4e39eca8
MK <01aldrn10@gmail.com>
2023-10-17 03:44:51 +0900

    s

--- Apple/Vue3/vuedongsan/src/App.vue
+++ Apple/Vue3/vuedongsan/src/App.vue
@@ -6,9 +6,10 @@
       <!-- 왼쪽은 하나하나의 데이터 오른쪽은 1씩 증가하는 정수가 된다 -->
     </div>
     <div v-for="( ele, idx ) in products" :key="idx">
+      <img :src= "ele.img" class="roomimg">
       <h4>{{ products[idx].name }}</h4>
       <p>{{ ele.price }}만원</p>
-      <button v-on:click="num[idx]++">허위매물신고</button>
+      <button v-on:click="btnNum(idx)">허위매물신고</button>
       <span>신고수: {{ num[idx] }}</span>
     </div>
   </div>
@@ -22,16 +23,16 @@ export default {
     return {
       메뉴들: ["Home", "Shop", "About"],
       products: [
-      { name: '역삼동원룸', price: '50'},
-      { name: '천호동원룸', price: '20'},
-      { name: '마포구원룸', price: '40'}
+      { name: '역삼동원룸', price: '50', img: require('./assets/room0.jpg')},
+      { name: '천호동원룸', price: '20', img: require('./assets/room1.jpg')},
+      { name: '마포구원룸', price: '40', img: require('./assets/room2.jpg')}
       ],
-      num : [0,0,0]
+      num : [1,0,0],
     };
   },
   methods: {
-    btnNum(){
-      this.num += 1;
+    btnNum(idx){
+      this.num[idx] += 1;
     }
   },
   components: {},
@@ -52,5 +53,9 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+.roomimg{
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
