6193f04e84786f14a619e07dc15eaf54ccbdfcc0
MK <01aldrn10@gmail.com>
2023-10-21 02:19:50 +0900

    Update 06. 표준 내장 객체-3.js

--- FastCampus/AIO/3.JAVASCRIPT/00.study/06. 표준 내장 객체-3.js
+++ FastCampus/AIO/3.JAVASCRIPT/00.study/06. 표준 내장 객체-3.js
@@ -1,1 +1,44 @@
 ///// 날짜
+// const date = new Date()
+// 월은 제로베이스 넘버로 출력
+
+// 지정 날자
+const date = new Date(2023,10,20,12,57,30)
+console.log(d1); // 2023년 11월 20일 12:57:30
+
+// .getFullYear() .setFullYear()
+// 날짜 인스턴스의 '연도'를 반환하거나 지정
+console.log(date.getFullYear()); // 2023
+data.setFullYear(2024);
+console.log(date.setFullYear()); // 2024
+
+// .getMonth() .setMonth()
+// 날짜 인스턴스의 '월'을 반환하거나 지정
+// 0부터 시작(zero-based-numbering)
+console.log(date.getMonth()); // 11
+data.getMonth(0); // 0
+console.log(date.getMonth()); // 1
+
+//.getDate() .setDate()
+// 날짜 인스턴스의 '일'을 반환하거나 지정
+console.log(date.getDate()); // 20
+
+// .getHours() .setHours()
+// 날짜 인스턴스의 '시간'을 반환하거나 지정
+console.log(date.getHours()); // 12
+
+// .getMinutes() .setMinutes()
+// 날짜 인스턴스의 '분'을 반환하거나 지정
+console.log(date.getMinutes()); // 57
+
+// .getSeconds() .setSeconds()
+// 날짜 인스턴스의 '초'를 반환하거나 지정
+console.log(date.getSeconds()); // 30
+
+// .getDay()
+// 날짜 인스턴스의 '요일'을 반환.
+
+
+
+
+
