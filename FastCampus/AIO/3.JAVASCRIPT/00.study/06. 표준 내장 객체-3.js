4f3e472038b68c4893e3f700f536013d8149c5f1
MK <01aldrn10@gmail.com>
2023-10-21 02:35:01 +0900

    s

--- FastCampus/AIO/3.JAVASCRIPT/00.study/06. 표준 내장 객체-3.js
+++ FastCampus/AIO/3.JAVASCRIPT/00.study/06. 표준 내장 객체-3.js
@@ -37,8 +37,28 @@ console.log(date.getSeconds()); // 30
 
 // .getDay()
 // 날짜 인스턴스의 '요일'을 반환.
+// 일월화수목금토 순으로 0-6 숫자로 반환
+console.log(date.getDay()); // 숫자로 나온다
+function getDayKo(day){
+  switch(day){
+    case 0 : return '일'
+    case 1 : return '월'
+    case 2 : return '화'
+    case 3 : return '수'
+    case 4 : return '목'
+    case 5 : return '금'
+    case 6 : return '토'
+  }
+}
+console.log(getDayKo(date.getDay()));
 
+// .getTime() .setTime()
+// '1970-01-01 00:00:00'(유닉스타임)부터 경과한,
+// 날짜 인스턴스의 '밀리초(ms)'로 반환하거나 지정
 
+// Date.now()
+// '1970-01-01 00:00:00'(유닉스타임)부터 경과한,
+// 메소드가 호출될 때의 '밀리초(ms)'로 반환
 
 
 
