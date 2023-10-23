e8ded72bfb6b2f1e6ba58b9f203fd7056cc0e187
MK <01aldrn10@gmail.com>
2023-10-23 16:33:05 +0900

    s

--- Apple/Vue3/blog/src/App.vue
+++ Apple/Vue3/blog/src/App.vue
@@ -1,15 +1,87 @@
 <template>
+<div>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
 
+  <div>
+    <h5>개발자의 블로그입니다.</h5>
+    <p>vue로 만들었음</p>
+  </div>
+  
+  <ComList :블로그글="블로그글" :idx="0" />
+  <ComList :블로그글="블로그글" :idx="1" />
+  <ComList :블로그글="블로그글" :idx="2" />
+
+</div>
 </template>
 
 <script>
+import ComList from './components/ComList.vue';
+import blogData from './assets/blog.js';
 export default {
-  name: 'App',
+  name: "App",
+  data(){
+    return{
+      블로그글 : blogData
+    }
+  },
   components: {
-  }
-}
+    ComList : ComList,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ Apple/Vue3/blog/src/components/ComList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- <div v-for="(ele,idx) in 블로그글" :key="idx">
+      <h5>{{ 블로그글[idx].title }}</h5>
+      <p>{{ 블로그글[idx].content }}</p>
+      <p>{{ 블로그글[idx].date }}</p>
+    </div>
+  </div> -->
+    <div>
+      <h5>{{ 블로그글[idx].title }}</h5>
+      <p>{{ 블로그글[idx].content }}</p>
+      <p>{{ 블로그글[idx].date }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'ComList',
+  props :{
+    블로그글:Array,
+    idx:Number
+  }
+}
+</script>
+
+<style>
+
+</style>
