3f65e8238943c31bd422d9392b08646171e52083
MK <01aldrn10@gmail.com>
2023-10-28 22:50:41 +0900

    s

--- FastCampus/AIO/3.JAVASCRIPT/00.study/09. DOM.js
+++ FastCampus/AIO/3.JAVASCRIPT/00.study/09. DOM.js
@@ -26,3 +26,46 @@
 
 
 ///// 생성, 조회, 수정
+document.createElement()
+// 메모리에만 존대하는 새로운 HTML 요소를 생성
+
+E.prepend() / E.append()
+// 노드를 요소의 첫 번째 / 마지마 자식으로 삽입
+
+E.remove()
+// 요소를 제거
+
+E.insertAdjacentElement()
+// 대상 요소의 지정한 위치에 새로운 요소 삽입
+// 위치값: beforebegin / afterend - 대상 요소의 이전 형제 / 다음 형제
+// 위치값: afterbegin / beforeend- 대상 요소의 내부 앞 / 뒤
+// 대상요소.insertAdjacentElement(위치, 새로운 요소)
+
+N.insertBefore()
+// 부모노드의 자식인 참조노드의 이전 형제로 노드를 삽입
+// 부모노드.insertBefore(노드,참조노드)
+
+N.contains()
+// 주어진 노드가 노드 자신을 포함한 후손인지 확인
+// 노드.contains(주어진 노드)
+
+N.textContent()
+// 노드의 모든 텍스트를 얻거나 변경
+
+E.innerHTML
+// 요소의 모든 HTMl 내용을 하나의 문자로 얻거나, 새로운 HTML을 지정
+
+E.dataset
+// 요소의 각 'data-' 속성 값을 얻거나 지정
+
+E.tagName
+// 요소의 태그 이름을 반환
+
+E.id
+// 요소의 'id' 속성 값을 얻거나 지정
+
+E.className
+// 요소의 'class' 속성 값을 얻거나 지정
+
+
+
