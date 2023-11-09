fe514ed8b8dbc89b172db1b62005467b8ac6771e
Min Kyung <01aldrn10@gmail.com>
2023-11-09 23:43:23 +0900

    s

---
+++ Apple/Vue3/instagram/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="header">
+      <ul class="header-button-left">
+        <li>Cancel</li>
+      </ul>
+      <ul class="header-button-right">
+        <li>Next</li>
+      </ul>
+      <img src="./assets/logo.png" class="logo" />
+    </div>
+
+    <ComContainer :DataPost="DataPost"/>
+
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input type="file" id="file" class="inputfile" />
+        <label for="file" class="input-plus">+</label>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import ComContainer from './components/ComContainer.vue';
+import DataPost from './assets/data/DataPost.js';
+
+export default {
+  name: "App",
+  components: {
+    ComContainer:ComContainer
+  },
+  data(){
+    return{
+      DataPost : DataPost
+    }
+  }
+};
+</script>
+
+<style>
+@import 'css/app.css';
+</style>
---
+++ Apple/Vue3/instagram/src/components/ComContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ComPost />
+    <ComPost />
+    <ComPost />
+  </div>
+</template>
+
+<script>
+import ComPost from './ComPost.vue';
+
+export default {
+  components:{
+    ComPost:ComPost
+  },
+  props : {
+    DataPost : Array
+  }
+};
+</script>
+
+<style>
+</style>
+
