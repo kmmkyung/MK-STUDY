ec3edeb236d8a9f1f5ca513c937fec4b3e78f83e
Min Kyung <01aldrn10@gmail.com>
2023-12-17 19:14:06 +0900

    s

--- Apple/Node.js/FORUM/server.js
+++ Apple/Node.js/FORUM/server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
 
+// pubilc이라는 폴더 쓸거임~ 이라는 선언
 app.use(express.static(__dirname+'/pubilc'))
 
-app.listen(8080, () => {
+////////////////////////////////// 서버와 몽고디비 연결
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://admin:20131876@cluster0.jl1pweh.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+  app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
+})
+}).catch((err)=>{
+  console.log(err)
 })
 
 app.get('/', (요청, 응답) => {
@@ -14,6 +26,6 @@ app.get('/', (요청, 응답) => {
 }) 
 
 app.get('/news',(요청,응답)=>{
-  응답.send('데이터')
+  db.collection('post').insertOne({title:'어쩌구'})
 })
 
