74c54501ea40861dbd1bd3b3e7403ed49faef184
Min Kyung <01aldrn10@gmail.com>
2024-03-23 17:48:20 +0900

    Update test.js

--- ::Study/test.js
+++ ::Study/test.js
@@ -19,39 +19,46 @@ var nameArr = users.reduce((acc, cur)=>{
 
 
 
-let imgLoading = new Promise((resolve, reject) => {
-  img.addEventListener('load',function(){
-    resolve()
-  })
-  img.addEventListener('error',function(){
-    reject()
-  })
-})
+// let imgLoading = new Promise((resolve, reject) => {
+//   img.addEventListener('load',function(){
+//     resolve()
+//   })
+//   img.addEventListener('error',function(){
+//     reject()
+//   })
+// })
   
-  imgLoading.then(function(){
-    console.log('성공');
-  }).catch(()=>{
-    console.log('실패');
-  })
+//   imgLoading.then(function(){
+//     console.log('성공');
+//   }).catch(()=>{
+//     console.log('실패');
+//   })
 
-let promise2 = new Promise((resolve, reject) => {
-  $.ajax({
-    type : 'GET',
-    url : 'https://링크'
-  }).done(function(결과){
-    resolve(결과);
-  })
-})
+// let promise2 = new Promise((resolve, reject) => {
+//   $.ajax({
+//     type : 'GET',
+//     url : 'https://링크'
+//   }).done(function(결과){
+//     resolve(결과);
+//   })
+// })
 
-promise2.then(function(결과){
-  console.log(결과);
+// promise2.then(function(결과){
+//   console.log(결과);
   
-  return new Promise((resolve2) => {
-    $.ajax({
-      type:'GET', url:'https://링크2'}).done(function(결과2){
-      resolve2(결과2)
-    })
-  });
-}).then(function(){
-  console.log(결과2);
-})
+//   return new Promise((resolve2) => {
+//     $.ajax({
+//       type:'GET', url:'https://링크2'}).done(function(결과2){
+//       resolve2(결과2)
+//     })
+//   });
+// }).then(function(){
+//   console.log(결과2);
+// })
+async function fn1(){ // fn1 함수 자체가 promise가 되버린다!
+  return 1+1
+}
+
+fn1().then(function(add){
+  console.log( add+'성공');
+});
