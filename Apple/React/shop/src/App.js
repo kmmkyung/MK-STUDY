ae8d602f26ccb07996fa20e23b10486fd162ee26
Min Kyung <01aldrn10@gmail.com>
2024-03-31 00:12:19 +0900

    s

--- Apple/React/shop/src/App.js
+++ Apple/React/shop/src/App.js
@@ -5,13 +5,14 @@ import date from './date.js';
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom"
 import Detail from './routes/detail.js';
 import Card from './component/card.js';
+import axios from 'axios'
 
 function App() {
-  let [shoes] = useState(date);
-  let navigate = useNavigate()
+  let [shoes, setShoes] = useState(date);
+  let navigate = useNavigate();
+  let [click,setClick] = useState(1)
   return (
     <div className="App">
-
       <Navbar bg="dark" variant="dark">
         <Container>
         <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
@@ -35,6 +36,20 @@ function App() {
                 }
               </div>
             </div>
+            <button onClick={()=>{
+              console.log(click);
+              if(click+1<4){
+                axios.get('https://codingapple1.github.io/shop/data'+[click+1]+'.json').then((resolve)=>{
+                  let copy = [...shoes, ...resolve.data]
+                  console.log(copy);
+                  setShoes(copy)
+                  setClick(click+1)
+                }).catch(()=>{console.log('실패함');})
+              }
+              else if(click>=3){
+                alert('없음');
+              }
+            }}>더보기</button>
           </div>
         }></Route>
         <Route path="/detail/:id" element={<Detail shoes={shoes}></Detail>}></Route>
