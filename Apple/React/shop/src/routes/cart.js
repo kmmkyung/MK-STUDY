87705d913ba0df30a5358e7e5cdb665741f26591
Min Kyung <01aldrn10@gmail.com>
2024-04-07 00:34:46 +0900

    s

--- Apple/React/shop/src/routes/cart.js
+++ Apple/React/shop/src/routes/cart.js
@@ -1,8 +1,9 @@
-
 import {Table} from 'react-bootstrap'
-
+import {useSelector} from 'react-redux'
 
 function Cart(){
+  let stateCart = useSelector((state)=>{return state.cartData})
+  
   return(
     <div>
       <Table>
@@ -15,12 +16,16 @@ function Cart(){
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+          {
+            stateCart.map((a,i)=>{
+            return <tr key={i}>
+              <td>{a.id}</td>
+              <td>{a.name}</td>
+              <td>{a.count}</td>
+              <td>안녕</td>
+            </tr>
+            })
+          } 
         </tbody>
       </Table> 
     </div>
--- Apple/React/shop/src/store.js
+++ Apple/React/shop/src/store.js
@@ -1,7 +1,21 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore,createSlice} from '@reduxjs/toolkit'
+
+let userName = createSlice({ // state 저장 공간
+  name : 'user', // state 이름 작명하기
+  initialState : 'kim' // state 데이터
+})
+
+let cartData = createSlice({ // state 저장 공간
+  name : 'cart',
+  initialState : [
+  {id : 0, name : 'White and Black', count : 2},
+  {id : 2, name : 'Grey Yordan', count : 1}
+] 
+})
 
 export default configureStore({
   reducer:{
-    
+    userNameState : userName.reducer,
+    cartData: cartData.reducer
   }
 })
