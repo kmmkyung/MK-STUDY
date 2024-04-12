a5c1c1b7e01b7ce72632a16ddb2efb99d3e6f008
Min Kyung <01aldrn10@gmail.com>
2024-04-12 23:42:31 +0900

    s

---
+++ Nuxt.js/learn-nuxt-3/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage></NuxtPage>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
