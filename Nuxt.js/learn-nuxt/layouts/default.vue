0ae99773df9e77fbb530dcd8898e709c386f954b
Min Kyung <01aldrn10@gmail.com>
2024-04-13 19:38:48 +0900

    s

---
+++ Nuxt.js/learn-nuxt/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ $route.name }} 페이지</h1>
+    <NuxtLink to="/">홈페이지</NuxtLink>
+    <NuxtLink to="/main">메인페이지</NuxtLink>
+    <NuxtLink to="/product">상품페이지</NuxtLink>
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
+
+<script>
+  export default {
+    created(){
+      console.log(this.$route);
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Nuxt.js/learn-nuxt/pages/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>메인페이지</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async created(){
+    let response = await axios.get('http://localhost:3000/products')
+    console.log(response);
+  }
+}
+</script> 
+
+<style lang="scss" scoped>
+
+</style>
