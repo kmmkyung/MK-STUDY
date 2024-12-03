3d5eb2d69283961f6049462d83f48ba1733cd789
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-12-03 15:49:17 +0900

    Create 08. 클래스.ts

---
+++ FastCampus/AIO/5.TypeScript/ts/08. 클래스.ts
@@ -0,0 +1,10 @@
+// 클래스(Class)
+
+// 접근 제어자(Access Modifiers)
+// public - 어디에서나 자유롭게 접근 가능, 클래스 바디에서 생략 가능
+// protected - 나와 파생된 후손 클래스 내에서 접근 가능
+// private - 내 클래스에서만 접근 가능
+
+class UserA {
+  
+}
